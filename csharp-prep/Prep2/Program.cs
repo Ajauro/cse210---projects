972b7112546ee1c9ff0fd2d8dcfa4346b058d924
Ajauro <sou24013@byui.edu>
2024-06-14 22:19:45 -0400

    display grade percent

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -5,7 +5,43 @@ class Program
     static void Main(string[] args)
     {
 
-        //this ir prep 2
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string userGrade = Console.ReadLine();
+
+        int x = int.Parse(userGrade);
+
+        string letter = "";
+
+        if ( x >= 90)
+        {
+            letter = "A";
+        }
+        else if ( x >= 80)
+        {
+            letter = "B";
+        }
+        else if ( x >= 70)
+        {
+            letter = "C";
+        }
+        else if ( x >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is: {letter}");
+
+        if ( x >= 70)
+        {
+            Console.WriteLine("Congratulation, you are APROVED!");
+        }
+        else 
+        {
+            Console.WriteLine("Opss, you need to get your grade back!");
+        }
     }
 }
