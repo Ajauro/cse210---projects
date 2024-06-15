437ab2977f61f82ed1a826a23be33fbdc1e371a5
Ajauro <sou24013@byui.edu>
2024-06-15 01:26:09 -0400

    display magic number game

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -3,7 +3,44 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep3 World!");
-    }
+    {   
+        string answerUser;
+        do
+        {
+            //parte 1 and 2
+            //Console.WriteLine("What is the magic number? ");
+            //int number = int.Parse(Console.ReadLine());
+
+            // parte 3, with random number
+            Random randomGenerator = new Random();
+            int number = randomGenerator.Next(1, 101);
+
+            int guess = -1;
+            int attempts = 0;
+
+            while (guess != number)
+            {
+                Console.Write("What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
+                attempts++;
+
+                if (guess < number)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (guess > number)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+
+                    Console.WriteLine($"You have made {attempts} attempts.");
+                }
+            }
+        Console.WriteLine("Would you like to play again? yes/no: ");
+        answerUser = Console.ReadLine().ToLower();
+        } while (answerUser == "yes");
+    }   
 }
