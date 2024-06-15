75c944b980baac32fb0c8d3b78a4e81999e6d9a8
Ajauro <sou24013@byui.edu>
2024-06-14 20:59:28 -0400

    changed to display the name

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your first name? ");
+        string first_name = Console.ReadLine();
+        
+        Console.Write("What is your last name? ");
+        string last_name = Console.ReadLine();
+        Console.WriteLine(); //blank line
+        Console.Write($"Your name is {last_name}, {first_name} {last_name}.");
     }
 }
