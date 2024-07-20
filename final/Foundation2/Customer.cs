3fd3e5872056d6ee78abe5c927de2563a3439464
Ajauro <sou24013@byui.edu>
2024-07-20 12:27:41 -0400

    fundation 2

--- final/Foundation2/Customer.cs
+++ final/Foundation2/Customer.cs
@@ -1,5 +1,7 @@
 using System;
 
+
+//this. é uma palavra chave que se refere a instancia atual da classe, usada para acessar membros da propria classe
 public class Customer
 {
     private string _name;
@@ -7,7 +9,7 @@ public class Customer
 
     public Customer(string name, Address address)
     {
-        this._name = name;
+        this._name = name; //this melhora a clareza do código
         this._address = address;
     }
 
